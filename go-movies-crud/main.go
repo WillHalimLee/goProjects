14a0803fbd6b2ddff2a86bc5c2493a335cfc53ed
Halim Lee <leehalim10@gmail.com>
2024-04-16 20:33:21 -0700

    adding current projects

---
+++ go-movies-crud/main.go
@@ -0,0 +1,38 @@
+package main
+
+import(
+	"fmt"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"` 
+}
+
+
+type Director struc {
+	Firstname string `json:"firstname"` 
+	Lastname string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.newRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovies).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deletMovie).Methods("DELETE")
+
+	
+}
